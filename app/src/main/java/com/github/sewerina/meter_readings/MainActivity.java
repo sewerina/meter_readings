b0b6dc66407743b4bcb3cfc5d60ee8d59e87758f
elena <sewerina.ea@gmail.com>
2019-07-02 17:40:53 +0300

    read from SharedPref in holder

--- app/src/main/java/com/github/sewerina/meter_readings/MainActivity.java
+++ app/src/main/java/com/github/sewerina/meter_readings/MainActivity.java
@@ -1,14 +1,7 @@
 package com.github.sewerina.meter_readings;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,25 +9,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String KEY_PREF_COLDWATER = "coldWater";
+    private static final String KEY_PREF_HOTWATER = "hotWater";
+    private static final String KEY_PREF_DRAINWATER = "drainWater";
+    private static final String KEY_PREF_ELECTRICITY = "electricity";
+    private static final String KEY_PREF_GAS = "gas";
     private RecyclerView mRecyclerView;
     private FloatingActionButton mAddReadingFaB;
     private ReadingAdapter mAdapter;
     private MainViewModel mViewModel;
-    private static final String TAG = "MainActivity";
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         mViewModel.getReadings().observe(this, new Observer<List<ReadingEntity>>() {
@@ -92,17 +102,43 @@ public class MainActivity extends AppCompatActivity {
         private TextView mDrainWaterTv;
         private TextView mElectricityTv;
         private TextView mGasTv;
+
+        private LinearLayout mColdWaterLl;
+        private LinearLayout mHotWaterLl;
+        private LinearLayout mDrainWaterLl;
+        private LinearLayout mElectricityLl;
+        private LinearLayout mGasLl;
+
         private ReadingEntity mReadingEntity;
 
         public ReadingHolder(@NonNull View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+
+            mColdWaterLl = itemView.findViewById(R.id.ll_coldWater);
+            mHotWaterLl = itemView.findViewById(R.id.ll_hotWater);
+            mDrainWaterLl = itemView.findViewById(R.id.ll_drainWater);
+            mElectricityLl = itemView.findViewById(R.id.ll_electricity);
+            mGasLl = itemView.findViewById(R.id.ll_gas);
+
             mDateTv = itemView.findViewById(R.id.tv_date);
             mColdWaterTv = itemView.findViewById(R.id.tv_coldWater);
             mHotWaterTv = itemView.findViewById(R.id.tv_hotWater);
             mDrainWaterTv = itemView.findViewById(R.id.tv_drainWater);
             mElectricityTv = itemView.findViewById(R.id.tv_electricity);
             mGasTv = itemView.findViewById(R.id.tv_gas);
+
+            boolean isColdWater = mSharedPreferences.getBoolean(KEY_PREF_COLDWATER, true);
+            boolean isHotWater = mSharedPreferences.getBoolean(KEY_PREF_HOTWATER, true);
+            boolean isDrainWater = mSharedPreferences.getBoolean(KEY_PREF_DRAINWATER, true);
+            boolean isElectricity = mSharedPreferences.getBoolean(KEY_PREF_ELECTRICITY, true);
+            boolean isGas = mSharedPreferences.getBoolean(KEY_PREF_GAS, true);
+
+            mColdWaterLl.setVisibility(isColdWater ? View.VISIBLE : View.GONE);
+            mHotWaterLl.setVisibility(isHotWater ? View.VISIBLE : View.GONE);
+            mDrainWaterLl.setVisibility(isDrainWater ? View.VISIBLE : View.GONE);
+            mElectricityLl.setVisibility(isElectricity ? View.VISIBLE : View.GONE);
+            mGasLl.setVisibility(isGas ? View.VISIBLE : View.GONE);
         }
 
         void bind(ReadingEntity entity) {
