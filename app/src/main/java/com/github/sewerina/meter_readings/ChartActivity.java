dfd4bcb6e2215f315e221f1f26cfd9a20eb6b4db
elena <sewerina.ea@gmail.com>
2019-07-04 16:39:08 +0300

    set some colors for BarChart

--- app/src/main/java/com/github/sewerina/meter_readings/ChartActivity.java
+++ app/src/main/java/com/github/sewerina/meter_readings/ChartActivity.java
@@ -1,5 +1,7 @@
 package com.github.sewerina.meter_readings;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -40,6 +43,15 @@ public class ChartActivity extends AppCompatActivity {
     }
 
     private void showChart(List<ReadingEntity> readings) {
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setCenterAxisLabels(true);
+
+        mChart.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+        Description description = new Description();
+        description.setText("Данные по месяцам");
+        description.setTextAlign(Paint.Align.RIGHT);
+        mChart.setDescription(description);
+
         List<BarEntry> entriesGroupColdWater = new ArrayList<>();
         List<BarEntry> entriesGroupHotWater = new ArrayList<>();
         List<BarEntry> entriesGroupDrainWater = new ArrayList<>();
@@ -55,23 +67,29 @@ public class ChartActivity extends AppCompatActivity {
         }
 
         BarDataSet setColdWater = new BarDataSet(entriesGroupColdWater, "Cold water");
+        setColdWater.setColor(Color.BLUE);
+
         BarDataSet setHotWater = new BarDataSet(entriesGroupHotWater, "Hot water");
+        setHotWater.setColor(Color.RED);
+
         BarDataSet setDrainWater = new BarDataSet(entriesGroupDrainWater, "Drain water");
+        setDrainWater.setColor(Color.DKGRAY);
+
         BarDataSet setElectricity = new BarDataSet(entriesGroupElectricity, "Electricity");
+        setElectricity.setColor(Color.YELLOW);
+
         BarDataSet setGas = new BarDataSet(entriesGroupGas, "Gas");
+        setGas.setColor(Color.CYAN);
 
         BarData data = new BarData(setColdWater, setHotWater, setDrainWater, setElectricity, setGas);
-        float groupSpace = 0.08f;
-        float barSpace = 0.02f; // x2 dataset
-        float barWidth = 0.35f; // x2 dataset
-// (0.02 + 0.45) * 2 + 0.06 = 1.00 -> interval per "group"
+        float groupSpace = 0.05f;
+        float barSpace = 0.02f; // x5 dataset
+        float barWidth = 0.17f; // x5 dataset
+// (0.02 + 0.17) * 5 + 0.05 = 1.00 -> interval per "group"
         data.setBarWidth(barWidth); // set the width of each
         mChart.setData(data);
         float fromX = readings.get(0).date.getTime();
         mChart.groupBars(0, groupSpace, barSpace); // perform the "explicit" grouping
         mChart.invalidate(); // refresh
-
-        XAxis xAxis = mChart.getXAxis();
-        xAxis.setCenterAxisLabels(true);
     }
 }
